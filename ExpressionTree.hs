41bd62b6d68c51f08751fe956108fc311430333e
Keichi TAKAHASHI <keitix.keitix@gmail.com>
2011-06-03 15:37:16 +0900

    Added explicit declaration of instance Show Expression

--- ExpressionTree.hs
+++ ExpressionTree.hs
@@ -2,6 +2,14 @@ module ExpressionTree where
 
 data Operators = Add | Sub | Mul | Div | Pow deriving (Eq)
 
+opWeight = [(Add, 1), (Sub, 1), (Mul, 2), (Div, 2), (Pow, 3)]
+
+getOpWeight :: Operators -> Integer
+getOpWeight op =
+    case lookup op opWeight of
+        Just w  ->  w
+        Nothing ->  0
+
 instance Show Operators where
     show Add = "+"
     show Sub = "-"
@@ -14,4 +22,11 @@ data Expression = Number Integer
     | Op Operators Expression Expression
     | Func String Expression
 
-    deriving (Eq, Show)
+    deriving (Eq)
+
+instance Show Expression where
+    show (Symbol x)       = x
+    show (Number x)       = show x
+    show (Func name e)    = name ++ "(" ++ show e ++ ")"
+    show (Op op e1 e2)    = show e1 ++ show op ++ show e2
+
